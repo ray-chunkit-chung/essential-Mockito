c9643d297552253fa49dc983fb292e6bcd79abe2
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-07-13 11:04:33 +0900

    add junit assumption example

--- spring-boot/src/test/java/com/example/springboot/HelloTest.java
+++ spring-boot/src/test/java/com/example/springboot/HelloTest.java
@@ -1,12 +1,14 @@
 package com.example.springboot;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class HelloTest {
 
+  // Hello world example
   @Test
   @DisplayName("say hello string matches.")
   public void testSayHello() {
@@ -14,4 +16,12 @@ public class HelloTest {
     assertEquals("hello", hello.sayHello());
   }
 
+  // Assumption example. To determine to run test or not: Assumption true -> run test
+  @Test
+  @DisplayName("say hello string matches if hello is not null.")
+  public void testSayHelloWithAssumption() {
+    Hello hello = new Hello();
+    assumeTrue(hello != null);
+    assertEquals("hello", hello.sayHello());
+  }
 }
