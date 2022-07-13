af0160ebd3234e267ad7506920095717ef2d3625
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-07-13 17:08:32 +0900

    add mockito basic

---
+++ spring-boot/src/test/java/com/example/springboot/CalculateMethodsMockitoTest.java
@@ -0,0 +1,28 @@
+package com.example.springboot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CalculateMethodsMockitoTest {
+
+  @Mock
+  CalculateMethods calculateMethods;
+
+  @BeforeEach
+  public void setupMocks() {
+    Mockito.when(calculateMethods.divide(6, 3)).thenReturn(2.0);
+  }
+
+  @Test
+  public void testDivide() {
+    assertEquals(2.0, calculateMethods.divide(6, 3));
+  }
+
+}
