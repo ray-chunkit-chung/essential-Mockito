a6702273f1ee7fa46ac27e748b12588509f002a8
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-07-13 12:08:56 +0900

    add emum test example but failed

--- spring-boot/src/test/java/com/example/springboot/HelloDependInjectTest.java
+++ spring-boot/src/test/java/com/example/springboot/HelloDependInjectTest.java
@@ -4,17 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-
-// Test with Dependency injection
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+// import org.junit.jupiter.params.provider.EnumSource;
 
 @ExtendWith(HelloParameterResolver.class)
 public class HelloDependInjectTest {
 
+  // Test with Dependency injection
   @Test
   @DisplayName("say hello string matches with dependency injection.")
   public void testSayHelloDedendencyInjection(Hello hello) {
     assertEquals("hello", hello.sayHello());
   }
+
+  // Test with Parametrized test
+  @ParameterizedTest
+  @ValueSource(strings = { "hello", "hello", "hello" })
+  @DisplayName("say hello string matches with parametrized test.")
+  public void testSayHelloParametrized(String str, Hello hello) {
+    assertEquals(str, hello.sayHello());
+  }
+
+  // Test with Enum
+  // @ParameterizedTest
+  // @EnumSource(value = Hello.class, names = {"hello", "hello", "hello"})
+  // @DisplayName("say hello string matches with enum.")
+  // public void testSayHelloEnum(Hello hello) {
+  // assertEquals("hello", hello.sayHello());
+  // }
 
 }
