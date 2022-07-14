e17cb96aa97f0c8964b560e7cdddb7b6d7a366c1
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-07-14 16:29:39 +0900

    add another Mockito example

---
+++ spring-boot/src/test/java/com/example/springboot/StockTest.java
@@ -0,0 +1,30 @@
+package com.example.springboot;
+
+// Modify from https://www.tutorialspoint.com/mockito/mockito_first_application.htm
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class StockTest {
+
+  @Mock
+  Stock stock;
+
+  @BeforeEach
+  public void setupMocks() {
+    Mockito.when(stock.getName()).thenReturn("Google");
+  }
+
+  @Test
+  public void testGetName() {
+    assertEquals("Google", stock.getName());
+  }
+}
